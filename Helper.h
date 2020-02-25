5c7b841c03a74129c9c246420b66a6f494f1b1cc
Taryk228 <61391016+Taryk228@users.noreply.github.com>
2020-02-25 14:25:58 +0200

    Update Helper.h

--- Helper.h
+++ Helper.h
@@ -8,7 +8,8 @@
 using namespace std;
 class Helper{
 public:
-  int timedateUclock(){//time date unit clock
+  char timedateUclock(int i){//time date unit clock
+    char tim[20]="";
     int y,mo,md,wd,h,mi,s;
     time_t t;
     time(NULL);
@@ -21,7 +22,77 @@ public:
     h=t_m->tm_hour;
     mi=t_m->tm_min;
     s=t_m->tm_sec;
-    
+    if(i==1){//date
+      if(wd==1){
+        tim[1]='M';
+        tim[2]='o';
+        tim[3]='n';
+      }
+      if(wd==2){
+        tim[1]='T';
+        tim[2]='u';
+        tim[3]='e';
+      }
+      if(wd==3){
+        tim[1]='W';
+        tim[2]='e';
+        tim[3]='d';
+      }
+      if(wd==4){
+        tim[1]='T';
+        tim[2]='h';
+        tim[3]='u';
+      }
+      if(wd==5){
+        tim[1]='F';
+        tim[2]='r';
+        tim[3]='i';
+      }
+      if(wd==6){
+        tim[1]='S';
+        tim[2]='a';
+        tim[3]='t';
+      }
+      if(wd==7){
+        tim[1]='S';
+        tim[2]='u';
+        tim[3]='n';
+      }
+      tim[4]=' ';
+      if(md<10){
+        tim[5]='0';
+        tim[6]=md+48;
+        tim[7]=46;
+      }else{
+        tim[5]=(md/10)+48
+        tim[6]=(md%10)+48;
+        tim[7]=46;
+      }
+      if(mo<10){
+        tim[8]='0';
+        tim[9]=mo+48;
+        tim[10]=46;
+      }else{
+        tim[8]=(mo/10)+48;
+        tim[9]=(mo%10)+48;
+        tim[10]=46;
+      }
+      tim[11]=(y/1000)+48;
+      tim[12]=(y/100%10)+48;
+      tim[13]=(y/10%100)+48;
+      tim[14]=(y%1000)+48;
+    } else {
+      tim[1]=(h/10)+48;
+      tim[2]=(h%10)+48;
+      tim[3]=59;//':'
+      tim[4]=(mi/10)+48;
+      tim[5]=(mi%10)+48;
+      tim[6]=59;//':'
+      tim[7]=(s/10)+48;
+      tim[8]=(s%10)+48;
+      }
+    }
+    return tim;
   }
 private:
 };
