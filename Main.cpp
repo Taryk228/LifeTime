e72f2a893d25b4a325f6f8e67f70b288088c1c3a
Taryk228 <61391016+Taryk228@users.noreply.github.com>
2020-02-26 22:38:24 +0200

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,5 @@
 #include"Helper.h"
-int CALLBACK wWinMain (HWND hWnd, HWND,PWSTR,LPCSTR){
+int CALLBACK wWinMain (HWND hWnd, HWND,PWSTR szCmdShow,int nCmdShow){
   MSG msg;
   HDC hdc;
   //taking program data
